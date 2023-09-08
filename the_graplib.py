9e9f71c5e93732c8aff757f3bf02320bb7605c34
Ivan Genik <IVGenik@rambler.ru>
2023-09-08 21:06:22 +0500

    Add files via upload

---
+++ the_graplib.py
@@ -0,0 +1,13 @@
+'''
+graphlib — Functionality to operate with graph-like structures
+https://docs.python.org/3/library/graphlib.html
+
+https://docs-python.ru/search/?text=graphlib&opt=0&searchid=2423578
+
+Класс TopologicalSorter(), модуль graphlib в Python
+https://docs-python.ru/standart-library/klass-topologicalsorter-modulja-graphlib-python/
+
+Объект TopologicalSorter модуля graphlib в Python
+https://docs-python.ru/standart-library/klass-topologicalsorter-modulja-graphlib-python/obekt-topologicalsorter-modulja-graphlib/
+
+'''
